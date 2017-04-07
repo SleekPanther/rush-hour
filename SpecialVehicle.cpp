08bc864ec31878f3384ff1436cdc0a4ce6a85a6b
SleekPanther <macrobyte2@gmail.com>
2017-04-07 15:00:59 -0400

    fixed special vehicle cpp (somehow it was hpp)

--- SpecialVehicle.cpp
+++ SpecialVehicle.cpp
@@ -1,29 +1,7 @@
-#ifndef SpecialVehicle_hpp
-#define SpecialVehicle_hpp
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include "Vehicle.hpp"
-#include "HorizontalVehicle.hpp"
 #include "SpecialVehicle.hpp"
 
-using namespace std;
-
-class SpecialVehicle : public HorizontalVehicle {
-private:
-	//fields
-
-public:
-	SpecialVehicle();
-	~SpecialVehicle();
-
-};
-
-#endif
-
 SpecialVehicle::SpecialVehicle() {
-	cout << "\tnew special vehicle " << endl;
+	cout << "\t\tspecial vehicle (also print horizontal constuctor) " << endl;
 }
 
 SpecialVehicle::~SpecialVehicle() {
